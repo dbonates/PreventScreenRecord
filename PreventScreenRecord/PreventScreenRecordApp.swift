edf8e21af3722ff762c6f911f207b04d37eb5f9a
Daniel Bonates <daniel.bonates@wexinc.com>
2023-02-10 12:59:37 -0300

    Initial Commit

---
+++ PreventScreenRecord/PreventScreenRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreventScreenRecordApp.swift
+//  PreventScreenRecord
+//
+//  Created by Daniel Bonates on 10/02/23.
+//
+
+import SwiftUI
+
+@main
+struct PreventScreenRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
